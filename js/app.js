94ba25ee5624e8f30424d322d55287ca3c68ad31
Aran Wittert <aran@jobadder.com>
2019-09-16 14:12:15 +1000

    added card possibilities and generation of game board

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
+
 window.addEventListener('DOMContentLoaded', function() {
     document.getElementById("restart-button").addEventListener("click", function() {
+        let possibilities = 
+            ["python", "python",
+             "javascript", "javascript",
+             "java", "java",
+             "c++", "c++",
+             "go", "go",
+             "c", "c",
+             "rust", "rust",
+             "ruby", "ruby"];
+
         console.log("start!");
 
         document.getElementById("game-board").remove();
@@ -9,11 +23,19 @@ window.addEventListener('DOMContentLoaded', function() {
 
         for (let i = 0; i < 16; i++) {
             let card = document.createElement("div");
-            card.innerText = "Im card " + i;
+            let choice = possibilities.random();
+            let index = possibilities.indexOf(choice);
+            possibilities.splice(index, 1);
+            card.innerText = choice;
+            card.setAttribute("class", `card ${choice}`);
 
             gameBoard.appendChild(card);
         }
 
         document.getElementById("root").appendChild(gameBoard)
+
+        document.getElementById("game-board").addEventListener("click", function(e) {
+            console.log(e.target)
+        });
     });
-})
+});
