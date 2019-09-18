607c91835968961c1d407c080fac95fe1895026a
Aran Wittert <aran@jobadder.com>
2019-09-18 21:23:44 +1000

    latest

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+// Array function to choose a random element
 Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
 }
 
+// Wait until all page content loaded
 window.addEventListener('DOMContentLoaded', function() {
+    // Restart game button click event
     document.getElementById("restart-button").addEventListener("click", function() {
         let possibilities = 
             ["python", "python",
@@ -14,13 +17,14 @@ window.addEventListener('DOMContentLoaded', function() {
              "rust", "rust",
              "ruby", "ruby"];
 
-        console.log("start!");
-
+        // Remove and re-create game board element
         document.getElementById("game-board").remove();
-
         let gameBoard = document.createElement("div");
         gameBoard.setAttribute("id", "game-board");
 
+        let fragment = document.createDocumentFragment();
+
+        // Create card elements, assign random type, then append to doc fragment
         for (let i = 0; i < 16; i++) {
             let card = document.createElement("div");
             let choice = possibilities.random();
@@ -29,13 +33,22 @@ window.addEventListener('DOMContentLoaded', function() {
             card.innerText = choice;
             card.setAttribute("class", `card ${choice}`);
 
-            gameBoard.appendChild(card);
+            fragment.appendChild(card);
         }
+        // Append doc fragment to game board div
+        gameBoard.appendChild(fragment);
 
+        // Finally add back game board element to the page
         document.getElementById("root").appendChild(gameBoard)
 
+        // Game board click event
         document.getElementById("game-board").addEventListener("click", function(e) {
-            console.log(e.target)
+            let target = e.target;
+
+            // If card was pressed
+            if (target.className) {
+                console.log(target.className)
+            }
         });
     });
 });
