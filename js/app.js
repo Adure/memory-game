b2f7a581655856ccb490e430d6b3532b9358a4f4
Aran Wittert <aran@jobadder.com>
2019-10-02 00:54:05 +1000

    img elements for card faces

--- js/app.js
+++ js/app.js
@@ -30,17 +30,21 @@ window.addEventListener('load', function() {
             let card = document.createElement("div");
             let front = document.createElement("div");
             let back = document.createElement("div");
+            let img = document.createElement("img");
 
             let choice = possibilities.random();
             let index = possibilities.indexOf(choice);
             possibilities.splice(index, 1);
-            back.innerText = choice;
+
+            img.setAttribute("src", `./img/${choice}-logo.png`);
+            img.setAttribute("class", choice);
             container.setAttribute("class", "flip-container");
             container.setAttribute("onclick", "this.classList.toggle('flip');");
-            card.setAttribute("class", `card ${choice}`);
+            card.setAttribute("class", "card");
             front.setAttribute("class", `front ${choice}`);
             back.setAttribute("class", `back ${choice}`);
 
+            back.appendChild(img);
             card.appendChild(front);
             card.appendChild(back);
             container.appendChild(card);
@@ -50,22 +54,24 @@ window.addEventListener('load', function() {
         gameBoard.appendChild(fragment);
 
         // Finally add back game board element to the page
-        document.getElementById("root").appendChild(gameBoard)
+        document.getElementById("root").appendChild(gameBoard);
+
+        // Game board click event
+        document.getElementById("game-board").addEventListener("click", function(e) {
+            let target = e.target;
+
+            // If card was pressed
+            if (target.className) {
+                console.log(target.className);
+            }
+        });
     }
 
+    // Initialise game on load
     initGame();
 
+    // Re-initialise game on click of restart button
     document.getElementById("restart-button").addEventListener("click", function() {
         initGame();
-    });
-
-    // Game board click event
-    document.getElementById("game-board").addEventListener("click", function(e) {
-        let target = e.target;
-
-        // If card was pressed
-        if (target.className) {
-            console.log(target.className)
-        }
     });
 });
